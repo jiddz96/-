29b5fa3d1d718ba83f275e52dc865f6c25947a8f
jiddz96 <jiddz96@gmail.com>
2017-07-09 12:01:53 +0530

    Add files via upload

---
+++ Prob2.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Postive_Negative 
+{
+    public static void main(String[] args) 
+    {
+        int n;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number you want to check:");
+        n = s.nextInt();
+        if(n%2 == 0)
+		{ System.out.print("The number is even")}
+else { System.out.print("The number is odd")}
+}
+}
