4beb0c79fe70f33898ab5a19094b7ec0efa12058
jiddz96 <jiddz96@gmail.com>
2017-07-09 12:53:06 +0530

    Add files via upload

---
+++ Prob4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.lang.*;
+import java.io.*;
+
+public class Prob3{ 
+	 public static void main(String arg[]){
+Scanner sc = new sc(System.in);
+char ch;
+System.out.print("Enter the character: ");
+ch = sc.next().charAt(0);
+if ( ch = a || ch = e || ch = i || ch = o || ch = u)
+	System.out.print("The character is a vowel");
+else System.out.print("The character is a consonant");
+}
+}
+
